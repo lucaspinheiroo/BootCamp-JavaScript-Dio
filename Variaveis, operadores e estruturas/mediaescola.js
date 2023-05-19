52d9c44fa449848c9ef80424e39ac75442a70f80
Lucas Pinheiro <131314634+lucaspinheiroo@users.noreply.github.com>
2023-05-18 22:51:05 -0400

    média escola

---
+++ Variaveis, operadores e estruturas/mediaescola.js
@@ -0,0 +1,14 @@
+
+let nota1 = 7
+let nota2 = 7
+let nota3 = 9 
+
+let media = (nota1 + nota2 + nota3)/3
+
+if (media < 5) {
+    console.log('Reprovado')
+} else if (media >= 5 &&  media <=7) {
+    console.log('Recuperação')
+} else {
+    console.log('Você Passou')
+}
