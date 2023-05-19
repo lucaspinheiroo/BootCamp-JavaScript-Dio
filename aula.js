fe86d8295c087cf5fe5c7f6f3365fd8821377f1a
Lucas Pinheiro <131314634+lucaspinheiroo@users.noreply.github.com>
2023-05-18 21:00:08 -0400

    aula e desafio preço  do combustivel

---
+++ aula.js
@@ -0,0 +1,9 @@
+console.log('Olá'); 
+
+const valor_combustivel = 6.49
+const consumomedio = 8.5
+const distancia = 365
+
+let res = ((distancia/consumomedio)*valor_combustivel)
+
+console.log(`O valor gasto nessa viagem foi, ${res}`)
