fcc33e9292dd59e83d3844f476d52ab877d2a094
Lucas Pinheiro <131314634+lucaspinheiroo@users.noreply.github.com>
2023-05-20 23:45:25 -0400

    funcionamento do arrays e repetição do for

---
+++ listas (arrays)/aula1.js
@@ -0,0 +1,8 @@
+
+
+const alunos = ['Joao', 'Marina', 'Vitor']
+
+
+alunos.push('Lucas')
+alunos[4] = 'Vinicius'
+console.log(alunos)
---
+++ listas (arrays)/aula2.js
@@ -0,0 +1,7 @@
+
+
+const nome = 'Lucas dos Santos Pinheiro'
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i]);
+    
+}
